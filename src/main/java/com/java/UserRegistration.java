da6e7325bff7ff592ef76bbffa5dcf8de3520789
hhmadan <hhmadan03@gmail.com>
2022-11-21 00:42:19 +0530

    Initial Commit

---
+++ src/main/java/com/java/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.java;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO USER REGISTRATION WITH JUNIT PROGRAM");
+    }
+}
